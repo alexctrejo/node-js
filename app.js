7a3d6d0b714caf4c3648cad047cefbc391c8a4a0
Alexander Contreras <alexander@Alexanders-MacBook-Air.local>
2023-10-09 14:33:20 -0700

    servidor con express, primer despliegue

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
+// servir contenido estático
+app.use( express.static('./public/templated-roadtrip') )
+
+app.get('/generic', (req, res) => {
+  res.sendFile(__dirname + '/public/templated-roadtrip/generic.html')
+})
+
+app.get('/elements', (req, res) => {
+  res.sendFile(__dirname + '/public/templated-roadtrip/elements.html')
+})
+
+app.get('/personas', (req, res) => {
   res.send('Hello World')
 })
+
+app.get('*', (req, res) => {
+  res.send('Error 404 | Page not found')
+})
+
 
 app.listen(8080)
